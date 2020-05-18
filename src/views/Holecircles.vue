103c80b1ce8f764e2afd8b9f5665bdfd5b0927cc
Adam <adam@mjoro.com>
2020-05-18 18:36:38 +0200

    holcircles input validation, key bindings

--- src/views/Holecircles.vue
+++ src/views/Holecircles.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="wrapper">
+    <div class="wrapper" @keyup.enter="add"> 
+
       <section class="holecircles-input">
-        <input v-model="input" id="input" type="number">
+        <input v-model="input" id="input" type="text" maxlength="2">
         <div class="manage">
-          <button id="btn-add" v-on:click="add">+</button>
+          <button id="btn-add" @click="add">+</button>
           <button id="btn-remove" v-on:click="remove">-</button>
         </div>
+        <p>{{ errorMessage }}</p>
       </section>
 
       <section class="holecircles-output">
-        <button id="holecircles" v-on:click="input = hole" v-for="hole in holeCircles" :key="hole">{{hole}}</button>
+        <button id="holecircle" @click="input = hole" v-for="hole in holeCircles" :key="hole">{{hole}}</button>
       </section>
     </div>
 </template>
@@ -20,7 +22,8 @@ export default {
   data() {
     return {
       holeCircles: [],
-      input: null
+      input: null,
+      errorMessage: "error message"
     };
   },
   mounted() {
@@ -28,28 +31,42 @@ export default {
     setInterval(() => {
       this.holeCircles = JSON.parse(localStorage.getItem("holeCircles"));
     }, 500);
-
-    //Need to be debug this block, the code can't find the "add()" function
-    window.addEventListener("keypress", function(e) {
-      if (e.key == "Enter") {
-        this.add;
-        console.log("yeet");
-      }
-    });
   },
   methods: {
-    //Add new hole circle to the array
-    //Need some input check
+    //Add new holecircle to the array
     add() {
-      this.holeCircles.push(this.input);
-      this.holeCircles.sort();
-      localStorage.setItem("holeCircles", JSON.stringify(this.holeCircles));
+      if (this.isIncluded(this.input)) {
+        console.log("Holecircle already included");
+      } else if (!this.isValidInput(this.input)) {
+        console.log("Invalid input");
+      } else {
+        this.holeCircles.push(this.input);
+        this.holeCircles.sort();
+        localStorage.setItem("holeCircles", JSON.stringify(this.holeCircles));
+      }
     },
-    //Remove a selecteh hole circle from the array
-    //Need some input check
+    //Remove a selected holecircle from the array
     remove() {
-      this.holeCircles.splice(this.holeCircles.indexOf(this.input), 1);
-      localStorage.setItem("holeCircles", JSON.stringify(this.holeCircles));
+      if (!this.isIncluded(this.input)) {
+        console.log("Holcircle is not on the list");
+      } else if (!this.isValidInput(this.input)) {
+        console.log("Invalid input");
+      } else {
+        this.holeCircles.splice(this.holeCircles.indexOf(this.input), 1);
+        localStorage.setItem("holeCircles", JSON.stringify(this.holeCircles));
+      }
+    },
+    //Check the input is valid
+    isValidInput(input) {
+      if (input == null || isNaN(input) || input.toString().length != 2) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    //Check a holcircle is on (or not on) the list
+    isIncluded(input) {
+      return this.holeCircles.includes(input);
     }
   }
 };
@@ -61,8 +78,9 @@ export default {
     max-width: 960px;
     margin: 0 auto;
     section.holecircles-input {
+      position: sticky;
       display: grid;
-      grid-template-columns: 1fr 1fr;
+      grid-template-columns: 1fr 1fr 1fr;
       margin: 10px auto;
       width: 205px;
       align-items: center;
@@ -90,6 +108,12 @@ export default {
             color: lightgray;
           }
         }
+      }
+    }
+    section.holecircles-output {
+      #holecircle {
+        width: 80px;
+        height: 80px;
       }
     }
   }
